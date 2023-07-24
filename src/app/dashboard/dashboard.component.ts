accbb6fd41c91d06c11e56d67f99e7ceadca303d
Real-istic <RealSkaarj@gmx.de>
2023-07-24 11:43:18 +0200

    first integration of apexcharts in the dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as ApexCharts from 'apexcharts';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit() {
+    const options = {
+      chart: {
+        type: 'bar'
+      },
+      series: [{
+        name: 'sales',
+        data: [30, 40, 45, 50, 49, 60, 70, 91, 125]
+      }],
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    };
+
+    const chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+  }
 }
