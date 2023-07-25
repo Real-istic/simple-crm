082894a8721ad74e72b57cfa09e912e3f8176636
Real-istic <RealSkaarj@gmx.de>
2023-07-25 12:38:31 +0200

    more stats

--- src/app/dashboard-middle-section/dashboard-middle-section.component.ts
+++ src/app/dashboard-middle-section/dashboard-middle-section.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as ApexCharts from 'apexcharts';
 
 @Component({
   selector: 'app-dashboard-middle-section',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard-middle-section.component.scss']
 })
 export class DashboardMiddleSectionComponent {
+
+  ngOnInit() {
+    let options = {
+      stroke: {
+        curve: 'smooth',
+      },
+      chart: {
+        type: 'line'
+      },
+      colors: ["#447AE7", "#ffa200", "#00df77"],
+      series: [
+        {
+          name: "Users",
+          data: [66, 122, 333, 233, 554, 777, 987, 1337]
+        },
+        {
+          name: "Revenue",
+          data: [120, 329, 537, 436, 354, 945, 1450, 1158]
+        },
+        {
+          name: "Transactions",
+          data: [110, 220, 230, 440, 350, 760, 670, 1180]
+        }
+      ],
+      xaxis: {
+        categories: [2017, 2018, 2019, 2018, 2019, 2020, 2021, 2022, 2023]
+      }
+    }
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+    chart.render();
+  }
 
 }
