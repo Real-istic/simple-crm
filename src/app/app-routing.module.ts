a499f9eb00622fc201e2985753e446f6ae6061cd
Real-istic <RealSkaarj@gmx.de>
2023-09-20 09:12:03 +0200

    cut some unnecessary code out

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { DataTableComponent } from './data-table/data-table.component';
 import { LoginComponent } from './login/login.component';
-import { Router } from '@angular/router';
-import { FirebaseAuthModule } from './firebase-auth/firebase-auth.module';
 import { AuthGuard } from './auth-guard/auth.guard';
 
 
@@ -23,16 +21,5 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule {
-  router = inject(Router);
-  firebaseAuthModule: FirebaseAuthModule = inject(FirebaseAuthModule);
   authGuard: AuthGuard = inject(AuthGuard);
-
-  ngOnInit() {
-    // this.router.events.subscribe((event: any) => {
-    //   if ((event.url === '/user/:id' || event.url === '/user-data') && !this.firebaseAuthModule.isLoggedIn) {
-    //     this.router.navigate(['login']);
-    //     alert('You must be logged in to view this page');
-    //   }
-    // });
-  }
 }
